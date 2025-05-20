585e351bd41341e6d39cb1e67b44492fb0f0cb95
Damiano004 <damiano.purin@mat.tn.it>
2025-05-20 13:58:14 +0200

    funziona Yippi

--- shopEurekaServer/src/main/java/com/damiano/shopEurekaServer/ShopEurekaServerApplication.java
+++ shopEurekaServer/src/main/java/com/damiano/shopEurekaServer/ShopEurekaServerApplication.java
@@ -2,9 +2,10 @@ package com.damiano.shopEurekaServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnnableEurekaServer
+@EnableEurekaServer
 public class ShopEurekaServerApplication {
 
 	public static void main(String[] args) {
