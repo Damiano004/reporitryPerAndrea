ba505942521a1470ee6e7faaba61bc446bca8351
Damiano004 <damiano.purin@mat.tn.it>
2025-05-20 15:03:22 +0200

    creato purchases

---
+++ purchases/src/test/java/com/damiano/purchases/PurchasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damiano.purchases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
